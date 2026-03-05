9abe247be04895d0910680082c01ce91dc4e1051
Flo Greth <128581342+flogreth@users.noreply.github.com>
2026-03-05 13:52:03 +0100

    new code.py

--- software/default_codes/frisbee_3_pi_pico_w/code.py
+++ software/default_codes/frisbee_3_pi_pico_w/code.py
@@ -1,7 +1,25 @@
 ##   ------ TINKERTANK FRISBEE ------  ##
 
-import time
+import board, time, neopixel, touchio
+
+touch_pad = board.GP6
+touch = touchio.TouchIn(touch_pad)
+touch.threshold = 5000
+
+# Farben definieren
+BLACK = (0,0,0)
+GREEN = (0,255,0)
+
+bee = neopixel.NeoPixel(board.GP14, 1, brightness=1, auto_write=True)	# Helligkeit 0.8 = 80%
 
 while True:
     print("Diese Frisbee läuft seit",time.monotonic(),"Sekunden und wartet darauf, programmiert zu werden :-)")
-    time.sleep(1)
+    time.sleep(0.05)
+    
+    if touch.value:
+        bee.fill(GREEN)
+    else:
+        bee.fill(BLACK)
+        
+        
+        
